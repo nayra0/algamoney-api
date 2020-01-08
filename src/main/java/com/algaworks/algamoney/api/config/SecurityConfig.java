82a7188e6adaec384a245b6638e4472bde46c760
Nayra Oliveira <nayrabiaoliveira@gmail.com>
2020-01-07 23:13:23 -0300

    - Adicionado dependência do oauth2
    Criada classe ResourceServerConfig, AuthorizationServerConfig e
    OAuthSecurityConfig

--- src/main/java/com/algaworks/algamoney/api/config/SecurityConfig.java
+++ src/main/java/com/algaworks/algamoney/api/config/SecurityConfig.java
@@ -1,34 +1,34 @@
-package com.algaworks.algamoney.api.config;
-
-import org.springframework.context.annotation.Bean;
-import org.springframework.security.config.annotation.web.builders.HttpSecurity;
-import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
-import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
-import org.springframework.security.config.http.SessionCreationPolicy;
-import org.springframework.security.core.userdetails.User;
-import org.springframework.security.core.userdetails.UserDetailsService;
-import org.springframework.security.provisioning.InMemoryUserDetailsManager;
-
-@EnableWebSecurity
-public class SecurityConfig extends WebSecurityConfigurerAdapter {
-
+//package com.algaworks.algamoney.api.config;
+//
+//import org.springframework.context.annotation.Bean;
+//import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+//import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+//import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+//import org.springframework.security.config.http.SessionCreationPolicy;
+//import org.springframework.security.core.userdetails.User;
+//import org.springframework.security.core.userdetails.UserDetailsService;
+//import org.springframework.security.provisioning.InMemoryUserDetailsManager;
+//
+//@EnableWebSecurity
+//public class SecurityConfig extends WebSecurityConfigurerAdapter {
+//
 //	@Override
 //	protected void configure(AuthenticationManagerBuilder auth) throws Exception {
 //		auth.inMemoryAuthentication().withUser("admin").password("{noop}admin").roles("ROLE");
 //	}
-
-	@Bean	
-	public UserDetailsService userDetailsService() {
-		User.UserBuilder builder = User.withDefaultPasswordEncoder();
-		InMemoryUserDetailsManager manager = new InMemoryUserDetailsManager();
-		manager.createUser(builder.username("admin").password("admin").roles("ROLE").build());
-		return manager;
-	}
-
-	@Override
-	protected void configure(HttpSecurity http) throws Exception {
-		http.authorizeRequests().antMatchers("/categorias").permitAll().anyRequest().authenticated().and().httpBasic()
-				.and().sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS).and().csrf()
-				.disable();
-	}
-}
+//
+//	@Bean	
+//	public UserDetailsService userDetailsService() {
+//		User.UserBuilder builder = User.withDefaultPasswordEncoder();
+//		InMemoryUserDetailsManager manager = new InMemoryUserDetailsManager();
+//		manager.createUser(builder.username("admin").password("admin").roles("ROLE").build());
+//		return manager;
+//	}
+//
+//	@Override
+//	protected void configure(HttpSecurity http) throws Exception {
+//		http.authorizeRequests().antMatchers("/categorias").permitAll().anyRequest().authenticated().and().httpBasic()
+//				.and().sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS).and().csrf()
+//				.disable();
+//	}
+//}
