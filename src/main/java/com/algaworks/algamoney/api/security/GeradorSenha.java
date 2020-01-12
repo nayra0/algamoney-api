cfa8f75b9e042e7e95dee0cbcc4ce6748dddb118
Nayra Oliveira <nayrabiaoliveira@gmail.com>
2020-01-12 14:38:27 -0300

    Criado classe utilitária que usa BCryptPasswordEncoder para encodar string

---
+++ src/main/java/com/algaworks/algamoney/api/security/GeradorSenha.java
@@ -0,0 +1,15 @@
+package com.algaworks.algamoney.api.security;
+
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+
+public class GeradorSenha {
+
+	public static void main(String[] args) {
+
+		BCryptPasswordEncoder encoder = new BCryptPasswordEncoder();
+
+		System.out.println(encoder.encode("@ngul@r0"));
+
+	}
+
+}
